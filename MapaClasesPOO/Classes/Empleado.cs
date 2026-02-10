cccab15a7481c28953c9ce9bb4ffabf2e5a887cb
Asao7 <asahellorenzo63@gmail.com>
2026-02-09 21:14:21 -0400

    Proyecto Tarea POO Mapa de Clases ITLA

---
+++ MapaClasesPOO/Classes/Empleado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaClasesPOO.Clases
+{
+    public class Empleado : MiembroDeLaComunidad
+    {
+        public double Salario { get; set; }
+    }
+}
---
+++ MapaClasesPOO/Classes/ExAlumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaClasesPOO.Clases
+{
+    public class ExAlumno : MiembroDeLaComunidad
+    {
+        public int AnioGraduacion { get; set; }
+    }
+}
+
