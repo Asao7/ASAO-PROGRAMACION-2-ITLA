dd823fabd90132186b31949416d0f19c60da2f6e
Asao7 <asahellorenzo63@gmail.com>
2026-02-14 23:18:25 -0400

    CRUD Pets API completed

---
+++ Pets.API/DTOs/CreatePetDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Pets.API.DTOs
+{
+    public class CreatePetDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Species { get; set; }
+    }
+}
---
+++ Pets.API/DTOs/UpdatePetDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Pets.API.DTOs
+{
+    public class UpdatePetDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Species { get; set; }
+    }
+}
