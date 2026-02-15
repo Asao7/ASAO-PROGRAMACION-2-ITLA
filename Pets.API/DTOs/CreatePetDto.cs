ad21bbb3ae303c200daf8c492e2557f4aeda105a
Asahel Melec Lorenzo <asahellorenzo63@gmail.com>
2026-02-14 23:34:44 -0400

    Revert "CRUD Pets API completed"

--- Pets.API/DTOs/CreatePetDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pets.API.DTOs
-{
-    public class CreatePetDto
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Species { get; set; }
-    }
-}
