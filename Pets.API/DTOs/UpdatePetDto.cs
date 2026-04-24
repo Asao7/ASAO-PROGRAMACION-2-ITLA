99d1a4b2ac104de03479dd521e76d763de1291e9
Asao7 <asahellorenzo63@gmail.com>
2026-04-24 08:43:59 -0400

    Removed old Pets.API project

--- Pets.API/DTOs/UpdatePetDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pets.API.DTOs
-{
-    public class UpdatePetDto
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Species { get; set; }
-    }
-}
